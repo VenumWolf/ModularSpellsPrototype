02786c810fcbc0d7a5548c3279a59f7d149abb5f
VenumWolf <venumthewofl@gmail.com>
2020-07-16 08:45:35 -0500

    Created spell tests

---
+++ src/test/java/com/venumwolf/prototype/modularspells/core/spells/SpellTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2020 VenumWolf
+ *
+ * This file is part of Modular Spells.
+ *
+ * Modular Spells is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Modular Spells is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Modular Spells.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.venumwolf.prototype.modularspells.core.spells;
+
+import com.venumwolf.prototype.modularspells.core.spells.effects.Effect;
+import com.venumwolf.prototype.modularspells.core.spells.effects.EffectTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(MockitoJUnitRunner.class)
+
+class SpellTest {
+
+    Spell spell = new Spell();
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void addEffect() {
+
+    }
+}
