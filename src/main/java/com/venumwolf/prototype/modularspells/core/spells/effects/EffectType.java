51d873de64b3a5f4d3f67e7ee98059305a86dc88
VenumWolf <venumthewofl@gmail.com>
2020-07-16 08:11:15 -0500

    Created EffectType enum

---
+++ src/main/java/com/venumwolf/prototype/modularspells/core/spells/effects/EffectType.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2020 VenumWolf
+ *
+ * This file is part of Modular Spells.
+ *
+ * Modular Spells is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Modular Spells is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Modular Spells.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.venumwolf.prototype.modularspells.core.spells.effects;
+
+/**
+ * The EffectType dictates at what stage an effect should be applied.
+ */
+public enum EffectType {
+    PROJECTILE,
+    IMPACT,
+    CASTER,
+    AREA
+}
