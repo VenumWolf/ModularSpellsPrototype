75579f25376b21736185b926e4a5a815556fc9c3
VenumWolf <venumthewofl@gmail.com>
2020-07-24 11:13:10 -0500

    Created DefaultSpellEventListener test class

---
+++ src/test/java/com/venumwolf/prototype/modularspells/core/spells/listeners/DefaultSpellEventListenerTest.java
@@ -0,0 +1,45 @@
+package com.venumwolf.prototype.modularspells.core.spells.listeners;
+
+import com.venumwolf.prototype.modularspells.core.spells.Spell;
+import com.venumwolf.prototype.modularspells.core.spells.events.SpellPrecastEvent;
+import org.bukkit.entity.Entity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+class DefaultSpellEventListenerTest {
+
+    /**
+     * A mock Spell for testing.
+     */
+    @Mock
+    Spell spell;
+
+    /**
+     * A mock Entity for testing.
+     */
+    @Mock
+    Entity entity;
+
+    DefaultSpellEventListener listener;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        listener = new DefaultSpellEventListener();
+    }
+
+    @Test
+    void precastNotCancelled() {
+        SpellPrecastEvent event = new SpellPrecastEvent(spell, entity);
+        listener.onSpellPrecastEvent(event);
+        verify(spell).cast(entity);
+    }
+}
