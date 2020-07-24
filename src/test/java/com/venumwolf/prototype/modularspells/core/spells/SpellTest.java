8a5ac1123b40170d907e86e6bd7dc11b228de09f
VenumWolf <venumthewofl@gmail.com>
2020-07-24 07:31:37 -0500

    Static import all from Mockito

--- src/test/java/com/venumwolf/prototype/modularspells/core/spells/SpellTest.java
+++ src/test/java/com/venumwolf/prototype/modularspells/core/spells/SpellTest.java
@@ -37,9 +37,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 class SpellTest {
