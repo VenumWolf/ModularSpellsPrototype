643a04a4c318f8114b00d283700468fab51b7552
VenumWolf <venumthewofl@gmail.com>
2020-08-01 17:54:55 -0500

    Stubbed SpellCastListener

---
+++ src/main/java/com/venumwolf/prototype/modularspells/listeners/SpellCastListener.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2020 VenumWolf
+ *
+ * This file is part of Modular Spells.
+ *
+ * Modular Spells is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Modular Spells is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Modular Spells.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.venumwolf.prototype.modularspells.listeners;
+
+import com.venumwolf.prototype.modularspells.core.spells.Spell;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SpellCastListener implements Listener {
+
+    public SpellCastListener(JavaPlugin plugin, Spell spell) {
+        // Use hard-coded spell for now.
+
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+
+    }
+}
