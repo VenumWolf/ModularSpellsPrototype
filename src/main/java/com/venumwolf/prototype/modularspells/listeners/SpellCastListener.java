381b92c34b91735bee906a41df5f9d96e632038a
VenumWolf <venumthewofl@gmail.com>
2020-08-02 01:06:42 -0500

    Implemented SpellCastListener

--- src/main/java/com/venumwolf/prototype/modularspells/listeners/SpellCastListener.java
+++ src/main/java/com/venumwolf/prototype/modularspells/listeners/SpellCastListener.java
@@ -20,20 +20,59 @@
 package com.venumwolf.prototype.modularspells.listeners;
 
 import com.venumwolf.prototype.modularspells.core.spells.Spell;
+import org.bukkit.NamespacedKey;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SpellCastListener implements Listener {
 
-    public SpellCastListener(JavaPlugin plugin, Spell spell) {
-        // Use hard-coded spell for now.
+    /**
+     * The plugin to which this class is associated.  The plugin is needed for creating NamespacedKey objects for
+     * persistent data lookups.
+     */
+    private JavaPlugin plugin;
 
+    /**
+     * The spell to be cast.
+     * <p>
+     * For the time being, the spell is hard-coded as there is no spell retrieval system.  Such a system is planned in
+     * the near future.
+     * <p>
+     * TODO: Implement a per-entity spell retrieval system so spells can be picked and chosen for each spell caster.
+     */
+    private Spell spell;
+
+    public SpellCastListener(JavaPlugin plugin, Spell spell) {
+        this.plugin = plugin;
+        this.spell = spell;
     }
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
+        if (isItemUseAction(event.getAction()) && isWandItem(event.getItem())){
+            spell.trigger(event.getPlayer());
+            event.setCancelled(true);
+        }
+    }
 
+    private boolean isWandItem(ItemStack item) {
+        boolean isWand = false;
+        try {
+            ItemMeta meta = item.getItemMeta();
+            PersistentDataContainer data = meta.getPersistentDataContainer();
+            isWand = data.get(new NamespacedKey(plugin, "isWand"), PersistentDataType.BYTE) != 0;
+        } catch (NullPointerException e) {}
+        return isWand;
+    }
+
+    private boolean isItemUseAction(Action action) {
+        return ((action == Action.RIGHT_CLICK_AIR) || (action == Action.RIGHT_CLICK_BLOCK));
     }
 }
