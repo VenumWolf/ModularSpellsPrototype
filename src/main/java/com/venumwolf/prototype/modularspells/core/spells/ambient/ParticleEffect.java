29f4007d59277d4669efc9ec107266eb60ee11f6
VenumWolf <venumthewofl@gmail.com>
2020-09-22 19:57:55 -0500

    Created ParticleEffect interface

---
+++ src/main/java/com/venumwolf/prototype/modularspells/core/spells/ambient/ParticleEffect.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2020 VenumWolf
+ *
+ * This file is part of Modular Spells.
+ *
+ * Modular Spells is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Modular Spells is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Modular Spells.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.venumwolf.prototype.modularspells.core.spells.ambient;
+
+
+import org.bukkit.Location;
+
+/**
+ * Particle effects as preconfigured objects.  Allows for preconfigured particle systems that can be passed into other
+ * functions as arguments.
+ */
+public interface ParticleEffect {
+
+    /**
+     * Play the particle effect at the specified location.
+     * @param location The location to play the particle effect.
+     */
+    void play(Location location);
+}
